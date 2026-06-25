c5c64b611714a914dfa7bd9af4ae42d8c9491223
khushsharma18 <skkhushsharma@gmail.com>
2026-06-25 13:44:18 +0530

    InsufficientEthPayment error handle

--- contracts/TokenMarketplace.sol
+++ contracts/TokenMarketplace.sol
@@ -7,11 +7,16 @@ contract TokenMarketPlace {
     uint256 private reseverdOrderedTokens;
 
     error TokenMarketPlace_ZeroNumberOfTokens(uint256 numberOfTokens);
+    error TokenMarketPlace_InsufficientEthPayment(uint256 ExpectedPayment, uint256 ActualPayment);
 
        function buyTokensFromMarketPlace(uint256 numberOfTokens) external payable{
          if(numberOfTokens==0){
             revert TokenMarketPlace_ZeroNumberOfTokens(numberOfTokens);
-        
         }
+         if(numberOfTokens*TOKEN_PRICE>msg.value) {
+            revert TokenMarketPlace_InsufficientEthPayment(numberOfTokens*TOKEN_PRICE, msg.value);
+
+         }
+
     }
 }
